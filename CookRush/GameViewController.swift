4dfc1a6de285c513da61eab7f7b48073b131a0c3
patwyzlic <patrickwyzlic@gmail.com>
2022-08-15 21:43:06 -0400

    Creation of blank scene

--- CookRush/GameViewController.swift
+++ CookRush/GameViewController.swift
@@ -7,53 +7,29 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+                scene.scaleMode = .aspectFill
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
+                //Present the scene
+                view.presentScene(scene)
                     
-                    view.ignoresSiblingOrder = true
+                view.ignoresSiblingOrder = true
                     
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
+                //Display FPS on screen
+                view.showsFPS = true
+                //Show number of nodes on screen
+                view.showsNodeCount = true
+                //Draws physics bodies for detection
+                view.showsPhysics = true
             }
         }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
