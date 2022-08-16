7873550a7e0c35d26f723fc7a71d3ed85b4b1ace
patwyzlic <patrickwyzlic@gmail.com>
2022-08-15 22:18:43 -0400

    Sprite of spaceship added in to center of screen

--- CookRush/GameScene.swift
+++ CookRush/GameScene.swift
@@ -10,8 +10,21 @@ import SpriteKit
 class GameScene: SKScene {
     
     let myFirstNode = SKNode()
+    //Creates a red box node
+    let myFirstSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 200.0))
+    let myFirstTexturedSpriteNode = SKSpriteNode(imageNamed: "smallspaceship")
     
     override func didMove(to view: SKView) {
         addChild(myFirstNode)
+        
+        myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        myFirstSpriteNode.anchorPoint = CGPoint.zero
+        
+        //Initializing the sprite node
+        addChild(myFirstSpriteNode)
+        
+        myFirstTexturedSpriteNode.size = CGSize(width: 100.0, height: 100.0)
+        myFirstSpriteNode.addChild(myFirstTexturedSpriteNode)
     }
 }
